544796f30ae87a3cba1fafd20ab3eb6ed7d3ff60
DinoSage <dinosage5632@gmail.com>
2024-11-09 19:50:01 -0500

    [+] Basic Camera Follow Player

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    // -- Private Fields --
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.z = this.transform.position.z;
+        this.transform.position += pos;
+    }
+}
