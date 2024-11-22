8b1ec9900390117915e4c9839612d910c10546f2
DinoSage <dinosage5632@gmail.com>
2024-11-22 04:52:40 -0500

    [-] Remove Setting Stuff from World Object

--- Assets/Scripts/World/WorldObject.cs
+++ Assets/Scripts/World/WorldObject.cs
@@ -6,44 +6,6 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class WorldObject : MonoBehaviour
 {
-    // -- Serialize Fields --
-    [SerializeField]
-    private bool debugUpdates;
-
-    [SerializeField]
-    private bool prefix;
-
-    // -- Private Fields --
-    private Setting setting;
-
-    private string Process(string update)
-    {
-        string time = World.instance.GetTimeStr();
-        string location = (setting == null) ? "Unknown" : setting.name;
-        string str = (prefix) ? string.Format("At time {0} at {1}: {2}", time, location, update) : update;
-        if (debugUpdates) Debug.Log(str);
-
-        return str;
-    }
-
-    public void OnTriggerEnter2D(Collider2D collider)
-    {
-        Setting location = collider.gameObject.GetComponent<Setting>();
-        if (location != null)
-        {
-            this.setting = location;
-        }
-    }
-
-    public void OnTriggerExit2D(Collider2D collider)
-    {
-        Setting location = collider.gameObject.GetComponent<Setting>();
-        if (location != null && this.setting == location)
-        {
-            this.setting = null;
-        }
-    }
-
     // -- Events (To Subscribe To) --
     public delegate void ProxemUpdate(string update);
     public event ProxemUpdate Proxem1Trigger;
@@ -53,17 +15,17 @@ public class WorldObject : MonoBehaviour
     // -- Public Functions --
     public void UpdateProxem1(string update)
     {
-        Proxem1Trigger?.Invoke(Process(update));
+        Proxem1Trigger?.Invoke(update);
     }
 
     public void UpdateProxem2(string update)
     {
-        Proxem2Trigger?.Invoke(Process(update));
+        Proxem2Trigger?.Invoke(update);
     }
 
     public void UpdateProxem3(string update)
     {
-        Proxem3Trigger?.Invoke(Process(update));
+        Proxem3Trigger?.Invoke(update);
     }
 
     public void UpdateProxemAll(string update)
@@ -71,10 +33,5 @@ public class WorldObject : MonoBehaviour
         UpdateProxem1(update);
         UpdateProxem2(update);
         UpdateProxem3(update);
-    }
-
-    public Setting GetCurrentSetting()
-    {
-        return this.setting;
     }
 }
