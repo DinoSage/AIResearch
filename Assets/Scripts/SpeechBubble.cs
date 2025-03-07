cf45001c2f565ae2c19e79101943baadad3872a4
DinoSage <dinosage5632@gmail.com>
2025-03-06 21:20:41 -0500

    [+] Garble Text

--- Assets/Scripts/SpeechBubble.cs
+++ Assets/Scripts/SpeechBubble.cs
@@ -8,7 +8,7 @@ public class SpeechBubble : MonoBehaviour
 
     static readonly float SPEAK_DURATION = 3f;
     static readonly float TEXT_ROLL = 0.05f;
-
+    static readonly float GARBLE_PERCENT = 0.6f;
 
     // -- Serialize Fields & Public --
     [SerializeField]
@@ -16,9 +16,12 @@ public class SpeechBubble : MonoBehaviour
 
     // -- Internal --
     private IEnumerator coroutine = null;
+    private WorldObject wobj;
 
     private void Start()
     {
+        wobj = GetComponent<WorldObject>();
+
         output.gameObject.SetActive(false);
     }
 
@@ -34,6 +37,27 @@ public class SpeechBubble : MonoBehaviour
         coroutine = null;
         output.SetText("");
         output.gameObject.SetActive(false);
+
+        // world object updates
+        Debug.Log(text);
+        string garbled = GarbleText(text, GARBLE_PERCENT);
+        Debug.Log(garbled);
+        wobj.UpdateProxem3(text);
+        wobj.UpdateProxem2(garbled);
+        Debug.Log(garbled);
+    }
+
+    private string GarbleText(string text, float percent)
+    {
+        char[] chars = text.ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (Random.Range(0f, 1f) <= percent)
+            {
+                chars[i] = '#';
+            }
+        }
+        return new string(chars);
     }
 
     // -- External --
